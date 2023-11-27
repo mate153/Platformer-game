98bed55825acc9886aac26874f164dcba119d2c5
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2023-11-27 11:57:50 +0100

    [ UPDATE ] Routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+import Main from './components/Main'
+import Profile from './components/Profile'
+import Game from './components/Game'
+import Settings from './components/Settings'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+  return (    
+    <div className='main-container'>
+      <div className='navbar-component-container'>
+        <Navbar />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
+      <div className='components-container'>
+        <Routes>
+          <Route path='/' element={
+              <Main
+
+              />
+            } 
+          />
+
+          <Route path='/Profile' element={
+              <Profile
+
+              />
+            } 
+          />
+
+          <Route path='/Game' element={
+              <Game
+
+              />
+            } 
+          />
+
+          <Route path='/Settings' element={
+              <Settings
+
+              />
+            } 
+          />
+
+        </Routes>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+
+    
   )
 }
 
