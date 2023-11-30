fcae0fc4c6aa77578fcf8d9a285f22153ad9f1e4
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2023-11-30 10:57:12 +0100

    [ UPDATE ] Player movement added

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -17,26 +17,27 @@ function Game() {
         this.velocity = {
           x: 0,
           y: 1,
-        }
+        };
         this.height = 100;
-      }
+      };
 
       draw() {
         c.fillStyle = 'red';
         c.fillRect(this.position.x, this.position.y, 100, this.height);
-      }
+      };
 
       update() {
         this.draw();
+        this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
 
         if(this.position.y + this.height + this.velocity.y < canvas.height){
           this.velocity.y += gravity;
         }else {
           this.velocity.y = 0;
-        }        
-      }
-    }
+        };        
+      };
+    };
 
     const player = new Player({
       x: 0,
@@ -45,8 +46,23 @@ function Game() {
 
     const player2 = new Player({
       x: 400,
-      y: 0,
+      y: 100,
     });
+
+    const keys = {
+      d: {
+        pressed: false,
+      },
+      s: {
+        pressed: false,
+      },
+      a: {
+        pressed: false,
+      },
+      w: {
+        pressed: false,
+      }
+    };
 
     function animate(){
       window.requestAnimationFrame(animate);
@@ -57,13 +73,51 @@ function Game() {
       player.update();
       player2.update();
       
+      player.velocity.x = 0;
+
+      if(keys.d.pressed){
+        player.velocity.x = 2;
+      } else if(keys.a.pressed){
+        player.velocity.x = -2;
+      }
     };
     animate();
 
+
+    window.addEventListener('keydown', (event) => {
+      switch(event.key){
+        case 'd': 
+          keys.d.pressed = true;
+          break;
+        
+        case 'a':
+          keys.a.pressed = true;
+          break;
+
+        case 's':
+          keys.s.pressed = true;
+          break;
+
+        case 'w':
+          keys.w.pressed = true;
+          player.velocity.y = -15;
+          break;
+      }        
+    });
+
+    window.addEventListener('keyup', (event) => {
+      switch(event.key){
+        case 'd': 
+          keys.d.pressed = false;
+          break;
+        
+        case 'a':
+          keys.a.pressed = false;
+          break;
+      }        
+    });
+
   }, []);
-
-
-
 
 
   return (    
