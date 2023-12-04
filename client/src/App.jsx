d34735589d7155b4b9206ffb79238dff8eb06d8a
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2023-12-04 17:24:27 +0100

    [ UPDATE ] Login component added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,21 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Profile from './components/Profile';
 import Game from './components/Game';
 import Settings from './components/Settings';
+import Login from './components/Login';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [jwtToken, setJwtToken] = useState(null); 
 
-
-  return (    
+  return (
+    loggedIn
+    ?
     <div className='main-container'>
       <div className='navbar-component-container'>
         <Navbar />
@@ -50,8 +55,10 @@ function App() {
 
       </div>
     </div>
-
-    
+    :
+    <div className='login-container'>
+      <Login/>
+    </div>    
   )
 }
 
