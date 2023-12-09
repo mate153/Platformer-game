a3facafa4904f8e575c1a1d4649b5c2fc6e91eda
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2023-12-09 21:12:40 +0100

    [ UPDATE ] Register form added

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -4,10 +4,15 @@ import './style/Login.css';
 function Login({setUserData}) {
     const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
+    const [loginOrReg, setLoginOrReg] = useState(true);
     
 
     const handleLogInBtn = () => {
         setUserData({username: userName, password: password});
+    };
+
+    const handleLoginOrReg = () => {
+        setLoginOrReg(false);
     };
 
     return (
@@ -15,7 +20,8 @@ function Login({setUserData}) {
             <video className='video-container' autoPlay muted loop>
                 <source src='/welcome_background/main_background3.mp4' type='video/mp4'/>
             </video>
-            <div id="form-ui">
+            {loginOrReg ?
+                <div id="form-ui">
                 <form action="" method="post" id="form">
                     <div id="form-body">
                         <div id="welcome-lines">
@@ -35,13 +41,46 @@ function Login({setUserData}) {
                             <button id="submit-button" onClick={() => handleLogInBtn()}>Login</button>
                         </div>
                         <div id="forgot-pass">
-                            <a href="#">Forgot password?</a>
+                            <a onClick={() => handleLoginOrReg()}>Register here</a>
                         </div>
                         <div id="bar">
                         </div>
                     </div>
                 </form>
-            </div> 
+                </div> 
+             :
+                <div id="form-ui">
+                <form action="" method="post" id="form">
+                    <div id="form-body">
+                        <div id="welcome-lines">
+                            <div id="welcome-line-1">Solo</div>
+                            <div id="welcome-line-2">The</div>
+                            <div id="welcome-line-3">Cat</div>
+                        </div>
+                        <div id="input-area">
+                            <div className="form-inp">
+                                <input placeholder="Username" type="text"/>
+                            </div>
+                            <div className="form-inp">
+                                <input placeholder="Email" type="text" onChange={(event) => setUserName(event.target.value)}/>
+                            </div>
+                            <div className="form-inp">
+                                <input placeholder="Password" type="password" onChange={(event) => setPassword(event.target.value)}/>
+                            </div>
+                            <div className="form-inp">
+                                <input placeholder="Confirm password" type="password"/>
+                            </div>
+                        </div>
+                        <div id="submit-button-cvr">
+                            <button id="submit-button">Register</button>
+                        </div>
+                        <div id="forgot-pass">
+                            <a onClick={() => setLoginOrReg(true)}>Back</a>
+                        </div>
+                    </div>
+                </form>
+                </div>              
+             }
         </>
     )
 }
